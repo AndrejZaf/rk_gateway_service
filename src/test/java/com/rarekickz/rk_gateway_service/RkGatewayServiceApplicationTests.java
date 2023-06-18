7a8ff61e9cfc146625642be0042ca62e9796d041
Андреј Зафировски <Andrej.Zafirovski@students.finki.ukim.mk>
2023-06-18 19:54:06 +0200

    Initial commit

---
+++ src/test/java/com/rarekickz/rk_gateway_service/RkGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rarekickz.rk_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RkGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
